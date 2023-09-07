1dcd4242087c933f435d9fe779491620fae6d47f
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-09-07 11:09:15 +0100

    add 2-rectangle

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,18 @@
+/* A class Rectangle that defines a rectangle with a width and height.
+*/
+
+class Rectangle {
+    constructor(w, h) {
+        this.width = w;
+        this.height = h;
+
+        // if w or h is 0 or not positive integer, create an empty object
+        if (w <= 0 || h <= 0 || w % 1 !== 0 || h % 1 !== 0) {
+            return {};
+        }
+    }
+}
+
+
+
+module.exports = Rectangle;
