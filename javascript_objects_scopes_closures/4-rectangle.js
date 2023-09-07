9d934de891b7bdaa098485e6f085245d1b804fdc
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-09-07 21:54:59 +0100

    please forget this python syntax here

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -1,4 +1,4 @@
-class Rectangle:
+class Rectangle {
   constructor(w, h) {
     if (w <= 0 || h <= 0 || !Number.isInteger(w) || !Number.isInteger(h)) {
       // If w or h is <= 0 or not a positive integer, create an empty object
