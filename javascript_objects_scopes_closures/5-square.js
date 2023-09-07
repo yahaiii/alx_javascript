325b7bd24948424676190c722273b3d8fd492801
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-09-07 21:11:56 +0100

    modify 5-square, fix import

--- javascript_objects_scopes_closures/5-square.js
+++ javascript_objects_scopes_closures/5-square.js
@@ -1,6 +1,6 @@
 /*  a class Square that defines a square and *   inherits from Rectagle
 */
-const { Rectangle } = require('4-rectangle');
+const { Rectangle } = require('./4-rectangle');
 
 class Square extends Rectangle {
     constructor(size) {
