8923148f68685b6573a80b0d6468fe8596f88155
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-09-13 10:48:34 +0100

    add 2-starwars_count

---
+++ javascript-network/2-starwars_count.js
@@ -0,0 +1,41 @@
+const request = require('request');
+
+const url = process.argv[2];
+
+if (!url) {
+    console.error('Please enter a valid URL');
+    process.exit(1);
+}
+
+function fetchData(url) {
+    return new Promise((resolve, reject) => {
+        request.get(url, (error, response, body) => {
+            if (error) {
+                reject(error);
+            } else if (response.statusCode !== 200) {
+                reject(new Error('Failed to fetch movie details. Status code:', response.statusCode));
+            } else {
+                resolve(JSON.parse(body));
+            }
+        });
+    });
+}
+
+async function countFilmsWithWedgeAntilles(url) {
+    try {
+        const films = await fetchData(url);
+        const characterId =  18; // Wedge Antilles
+
+        // Filter movies with Wedge Antilles
+        const filmsWithWedgeAntilles = films.results.filter((film) => {
+            return film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${characterId}/`);
+        });
+
+        console.log(filmsWithWedgeAntilles.length);
+    } catch (error) {
+        console.error('Error:', error.message);
+        process.exit(1);
+    }
+}
+
+countFilmsWithWedgeAntilles(url);
