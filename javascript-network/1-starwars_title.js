0344ca3235aedcc862d34425dde4df54e8c20150
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-09-13 05:56:59 +0100

    add 1-starswars_title

---
+++ javascript-network/1-starwars_title.js
@@ -0,0 +1,30 @@
+const request =  require('request');
+
+const movieID = process.argv[2];
+
+if (!movieID) {
+    console.error('Please enter a valid movie id');
+    process.exit(1);
+}
+
+const api = `https://swapi-api.alx-tools.com/api/films/${movieID}`;
+
+response = request.get(api, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error.message);
+        process.exit(1);
+    }
+
+    if (response.statusCode !== 200) {
+        console.error('Failed to fetch movie details. Status code:', response.statusCode);
+        process.exit(1);
+    }
+
+    try {
+        const movie = JSON.parse(body);
+        console.log(`Title: ${movie.title}`);
+    } catch (parseError) {
+        console.error('Error parsing JSON response:', parseError.message);
+        process.exit(1);
+    }
+});
