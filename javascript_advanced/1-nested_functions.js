fafc5dfd5b2197272d69bb7fb890e3b7f4152704
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-09-14 15:13:12 +0100

    add 1-nested_functions

---
+++ javascript_advanced/1-nested_functions.js
@@ -0,0 +1,20 @@
+const alert = require('alert');
+
+
+const globalVariable  = 'Welcome';
+
+function outer () {
+    alert(globalVariable);
+    const course = 'Holberton';
+    function inner () {
+        alert(globalVariable + ' ' + course);
+        const exclamation = '!';
+
+        function inception () {
+            alert(globalVariable + ' ' + course + exclamation);
+        }
+        inception();
+    }
+    inner();
+}
+outer();
