f540d4e6e5e24253374bbd34b2c06b84c9f4d57a
Yahaya Aluke <52191233+yahaiii@users.noreply.github.com>
2023-09-14 15:16:41 +0100

    add 2-function_me

---
+++ javascript_advanced/2-function_me.js
@@ -0,0 +1,8 @@
+const alert = require('alert');
+function welcomeMessage (fullName) {
+    alert('Welcome ' + fullName);
+}
+
+const guillame = welcomeMessage('Guillame');
+const alex = welcomeMessage('Alex');
+const fred = welcomeMessage('Fred');
